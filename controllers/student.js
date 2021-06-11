742fc11f25418fe5f504f60c02aa7d8e140ac860
yousefnafea <yousef.nafea@ejust.edu.eg>
2021-06-11 20:27:27 +0200

    MARKING SYSTEM DONE

--- controllers/student.js
+++ controllers/student.js
@@ -2,7 +2,7 @@ const lodash = require('lodash');
 const sqlQuery = require('../config/db');
 const studentService = require('../services/student');
 const gpaSystem = require('../services/gpa');
-// const course = require('./course');
+const courseSystem = require('./course');
 
 module.exports = {
     getStudents: async (req, res) => {
@@ -27,75 +27,36 @@ module.exports = {
 
     getRegistration: async (req, res) => {
         // Get student id and semester
-        const stud_id = req["params"]["id"];
+        const stud_id = req.params.id;
         const student = await sqlQuery(`SELECT * FROM student WHERE id = '${stud_id}'`)
         // Fetch semester courses and place it in an array
-        const semester_courses = await sqlQuery(`SELECT id FROM course WHERE semester <= '${student[0]['semester']}'`)
-        let courses_id = [];
-        for(var i = 0; i < semester_courses.length; i++){
-            courses_id.push(semester_courses[i]['id']);
+        const pre_achieved = await sqlQuery(`SELECT prerequisite.course_id, count(prerequisite.course_id) as count FROM prerequisite JOIN (SELECT course_id FROM student JOIN registration on id = student_id WHERE mark >= 60 AND id = ${stud_id})as t on pre_id = t.course_id group by prerequisite.course_id;`)
+        var pre_taken = pre_achieved.map(i => i.course_id);
+        var x = pre_achieved.map(i => i.count);
+        var pre_of_courses = []
+        var count2 = []
+        if (pre_taken.length != 0){
+            pre_of_courses = await sqlQuery(`SELECT course_id, count(course_id) as count FROM prerequisite WHERE course_id in  (${pre_taken.join()}) GROUP BY course_id;`);
+            count2 = pre_of_courses.map(i => i.count);
+            }
+        for(var i = 0; i < count1.length; i++){
+            if(count1[i] == count2[i]){ available_pre.push(pre_taken[i])}
         }
-        // get prerequisites of available courses if any and place them in a hash, get previously taken courses and place them in an array
-        const pre = await sqlQuery(`SELECT p.id, GROUP_CONCAT( c.id SEPARATOR \', \') as "prerequisites" from (SELECT * from course left join prerequisite on course.id = prerequisite.course_id) p left join course c on p.pre_id = c.id WHERE p.id in ('${courses_id.join()}') group by p.id`)
-        const reg_courses = await sqlQuery(`SELECT course_id as id from registration WHERE student_id = \'${stud_id}'`);
-        let taken_courses = [];
-        for(var i = 0; i < reg_courses.length; i++){
-            taken_courses.push(reg_courses[i]['id']);
-        }
+        var no_pre = await sqlQuery(`SELECT id from course left join prerequisite ON course.id = prerequisite.course_id LEFT JOIN registration on id = registration.course_id  WHERE pre_id IS NULL and semester <= ${student[0].semester} AND (${stud_id} not in (SELECT student_id from registration WHERE course_id = course.id) OR (student_id = ${stud_id} AND mark <60))`)
+        no_pre = no_pre.map(i => i.id)
 
-        let pre_courses = {}
-        for (var i = 0; i < pre.length; i++) {
-            if((pre[i]['prerequisites']) == null){continue}
-            pre_courses[pre[i]['id']] = pre[i]['prerequisites'];
+        var available_pre = []
+        if (available_pre.length > 0){
+            var available_pre = await sqlQuery(`SELECT id FROM course LEFT JOIN registration ON course.id = registration.course_id WHERE course.id IN (${available_pre.join()}) AND course.semester <=${student[0].semester} AND (${stud_id} NOT IN (SELECT student_id FROM registration WHERE course_id = id) OR (student_id = ${stud_id} AND mark < 60));`)
         }
-
-        // iterate on the courses and make sure there is no prerequisites missing
-        var j = 0;
-        let course_cpy = [...courses_id];
-        for(var i = 0; i < courses_id.length; i++){
-            var course_pre = pre_courses[courses_id[i]]
-            if (typeof(course_pre) == 'undefined'){continue;}
-            if (typeof(taken_courses) == 'undefined'){
-                course_cpy.splice(i - j, 1);
-                j++;
-                continue;
-            }
-            if (course_pre.includes(',')){
-                course_pre = course_pre.split(',');
-                for(course in course_pre){
-                    if(!taken_courses.includes(course)){
-                        course_cpy.splice(i - j, 1);
-                        j++;
-                    } 
-                }
-            }
-            else{
-                if(taken_courses.includes(course_pre)){
-                    course_cpy.splice(i - j, 1);
-                    j++;
-                }
-            }
+        var available = no_pre.concat(available_pre.map(i => i.id));
+        if (available.length >0){
+            var available = await sqlQuery(`SELECT * FROM course WHERE id in (${available.join()})`)
         }
-
-        // remove already taken courses
-        var j = 0;
-        var course_cpy2 = [...course_cpy];
-        for(var i= 0; i < course_cpy.length; i++){
-            if (typeof(taken_courses) == 'undefined'){continue;}
-            if (taken_courses.includes(course_cpy[i])){
-                course_cpy2.splice(i - j, 1);
-                j++;
-            }
-        }
-        // only query if there are allowed courses
-        var allowed_courses = null;
-        if(course_cpy2.length > 0){
-            allowed_courses = await sqlQuery(`SELECT id,name FROM course WHERE id in(${course_cpy2.join()})`)
-        } 
         return res.render('student/register',{
                 activePath: 'students',
                 student: student[0],
-                allowedCourses: allowed_courses
+                allowedCourses: available
         });
     },
 
@@ -106,11 +67,17 @@ module.exports = {
         // check if the user chose more than one array and add the selected courses
         if (typeof(registered) == 'object'){
             for(var i = 0; i < registered.length; i++){
-                await sqlQuery(`INSERT INTO registration (student_id, course_id) values ('${student_id}', '${registered[i]}')`);
+                var exists = await sqlQuery(`SELECT * FROM registration WHERE student_id = ${student_id} AND course_id = ${registered[i]}`)
+                if (exists.length == 0){await sqlQuery(`INSERT INTO registration (student_id, course_id) values ('${student_id}', '${registered[i]}')`);}
+                else{await sqlQuery(`UPDATE registration SET mark = NULL WHERE student_id = ${student_id} AND course_id = ${registered[i]}`);}
             }
         }
         else{
-            await sqlQuery(`INSERT INTO registration (student_id, course_id) values ('${student_id}', '${registered}')`);
+            var exists = await sqlQuery(`SELECT * FROM registration WHERE student_id = ${student_id} AND course_id = ${registered}`)
+            console.log(exists)
+            console.log(exists.length);
+            if (exists.length == 0){await sqlQuery(`INSERT INTO registration (student_id, course_id) values ('${student_id}', '${registered}')`);}
+            else{console.log("HELLO");await sqlQuery(`UPDATE registration SET mark = NULL WHERE student_id = ${student_id} AND course_id = ${registered}`);}
         }
         return res.redirect('/students');
     },
@@ -118,16 +85,13 @@ module.exports = {
     getAddMarks: async (req, res) => {
         const stud_id = req["params"]["id"];
         const student = await sqlQuery(`SELECT * FROM student WHERE id = '${stud_id}'`)
-        const unmarked_courses = await sqlQuery(`SELECT course_id FROM registration JOIN course WHERE course_id = course.id AND registration.student_id = '${stud_id}' AND mark IS NULL;`)
-        console.log(unmarked_courses);
-        let courses_id = [];
-        for(var i = 0; i < unmarked_courses.length; i++){
-            courses_id.push(unmarked_courses[i]['course_id']);
-        }
-        var courses = []
-        if(unmarked_courses.length > 0){
-            courses = await sqlQuery(`SELECT * FROM course WHERE id in (${courses_id.join()})`);
-        }
+        var courses = await sqlQuery(`SELECT course_id as id, course.name FROM registration JOIN course WHERE course_id = course.id AND registration.student_id = '${stud_id}' AND mark IS NULL;`)
+        // let courses_id = unmarked_courses.map(i=> i.course_id);
+ 
+        // var courses = []
+        // if(unmarked_courses.length > 0){
+        //     courses = await sqlQuery(`SELECT * FROM course WHERE id in (${courses_id.join()})`);
+        // }
         return res.render('student/addMarks', {
             activePath: 'students',
             student: student[0],
@@ -137,21 +101,16 @@ module.exports = {
     },
 
     postAddMarks: async (req, res) => {
-        console.log("HEREE")
         var stud_id = req["params"]["id"];
         var marks = req.body;
         for(course in marks){
             if ((!isNaN(marks[course]) && (marks[course]!= ""))){
-                console.log(marks[course]);
-                console.log("YARE YARE")
                 await sqlQuery(`UPDATE registration SET mark = ${marks[course]} WHERE student_id = '${stud_id}' AND course_id = '${course}'`);
             }
         }
-        console.log("HIII")
         var gpa = await studentService.getTotalGPA(stud_id)
         var sem = await studentService.getSemester(stud_id)
         await sqlQuery(`UPDATE student SET gpa = ${gpa}, semester = ${sem} WHERE id = ${stud_id}`)
-
         return res.redirect('/students');
     },
 
